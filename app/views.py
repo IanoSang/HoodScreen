65c06c80fb37634cf9f0a87931e06787d1061c4c
IanoSang <ianosang18@gmail.com>
2022-06-18 13:24:43 +0300

    feat: views- adds user signup form to the views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import *
+from django.contrib.auth import login, authenticate
 
 
 # Create your views here.
 def index(request):
     return render(request, 'base.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignupForm()
+    return render(request, 'registration/signup.html', {'form': form})
