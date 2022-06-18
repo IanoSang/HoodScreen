b8691192bd2c0e1ca674c23dfe7b46127118046a
IanoSang <ianosang18@gmail.com>
2022-06-18 13:25:31 +0300

    feat: urls- adds a url path to signup page and the account login page

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
 ]
