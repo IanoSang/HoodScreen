657ad78af8e5404904a74089489d82bf669dfad3
IanoSang <ianosang18@gmail.com>
2022-06-19 14:50:23 +0300

    feat: urls- adds a url path to creating hood form and page displaying all the hoods

--- app/urls.py
+++ app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('account/', include('django.contrib.auth.urls')),
     path('profile/<username>', views.profile, name='profile'),
     path('profile/<username>/edit/', views.edit_profile, name='edit-profile'),
+    path('new-hood/', views.create_hood, name='new-hood'),
+    path('all-hoods/', views.hoods, name='hood'),
 ]
