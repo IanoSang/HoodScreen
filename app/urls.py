66ea59e918a60e44713b980a0f477566e675aa65
IanoSang <ianosang18@gmail.com>
2022-06-19 20:07:24 +0300

    feat: adds a url path to hood members page

--- app/urls.py
+++ app/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('all-hoods/', views.hoods, name='hood'),
     path('single_hood/<hood_id>', views.single_hood, name='single-hood'),
     path('<hood_id>/new-post', views.create_post, name='post'),
+    path('<hood_id>/members', views.hood_members, name='members'),
 ]
