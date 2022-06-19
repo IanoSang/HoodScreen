9d815694141871663a77d7a86e34ab0adb8a0fa9
IanoSang <ianosang18@gmail.com>
2022-06-19 19:50:21 +0300

    feat: forms- adds a form for posting new posts in the hood

--- app/forms.py
+++ app/forms.py
@@ -30,3 +30,9 @@ class BusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         exclude = ('user', 'neighbourhood')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
