df45c889fd11d262b5072eb3b5cf3d0128635147
IanoSang <ianosang18@gmail.com>
2022-06-19 14:47:42 +0300

    feat: feat: adds a neighbourhood form to create neighbourhoods in the app

--- app/forms.py
+++ app/forms.py
@@ -18,3 +18,9 @@ class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         exclude = ('user', 'neighbourhood')
+
+
+class NeighbourHoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        exclude = ('admin',)
